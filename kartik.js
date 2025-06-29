78afdb7024269a52a56b9891948c1c373479aa21
kartikpatel101 <kartikpatel000999p@gmail.com>
2025-06-29 13:20:03 +0530

    Add files via upload

---
+++ kartik.js
@@ -0,0 +1,19 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
+
+
+document.querySelector('form')?.addEventListener('submit', function () {
+  alert("Thanks for reaching out, Kartik will get back to you soon!");
+});
